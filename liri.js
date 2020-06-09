f0a4f72ec8761671e6ed65c30de30c16e36d07c1
Fernando Alvarado- <Fernandoahd@icloud.com>
2020-06-08 21:39:49 -0400

    function of concert this

--- liri.js
+++ liri.js
@@ -1,22 +1,27 @@
 const dotenv = require('dotenv');
 dotenv.config();
+const axios = require('axios').default;
+let command = process.argv[2]
 var keys = require("./keys.js");
 var Spotify = require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
-
+let input = []
+    for(i = 3; i < process.argv.length; i++) {
+        input.push(process.argv[i])
+    }
 // require("dotenv").config(); this is the shorter but harder to understand version. always opt for readability
 
 switch(command) {
     case "concert-this":
-            newConcert();
+            newConcert(input);
             break;
     
     case "spotify-this-song":
-            findSong();
+            findSong(input);
             break;
     
     case "movie-this":
-            movieTime();
+            movieTime(input);
             break;
     case "do-what-it-says":
             doNow();
@@ -24,6 +29,13 @@ switch(command) {
         
 }
 
-function concert() {
+function newConcert() {
+    const queryUrl = "https://rest.bandsintown.com/artists/" + input.join(" ") + "/events?app_id=codingbootcamp"
+    axios
+        .get(queryUrl)
+        .then(function(response) {
 
+        console.log(response.data);
+  })
+    
 }
